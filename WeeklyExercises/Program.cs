3d9f998ac018026ccb001b0805f787b244a071f6
Jake <176844671+LgndryJake@users.noreply.github.com>
2025-02-18 17:59:08 +0800

    initial commit - exercise 1

---
+++ WeeklyExercises/Program.cs
@@ -0,0 +1,13 @@
+﻿//EXERCISE 1
+string firstName = "Jacob", lastName = "Oliver";
+int age = 36;
+bool intStudent = false;
+int petCount = 1;
+
+Console.WriteLine($"First Name: {firstName}");
+Console.WriteLine($"Last Name: {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"International Student? {intStudent}");
+Console.WriteLine($"I have {petCount} pet(s)");
+
+//EXERCISE 2
