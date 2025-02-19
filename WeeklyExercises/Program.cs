b95d1e46db004e829826d45a48a7ea9fdbcf57eb
Jake <176844671+LgndryJake@users.noreply.github.com>
2025-02-19 21:29:28 +0800

    exercise 4

--- WeeklyExercises/Program.cs
+++ WeeklyExercises/Program.cs
@@ -40,7 +40,7 @@ else
     Console.WriteLine("Her name is longer, {0} letters versus {1} letters.", lilyNameLength, jakeNameLength);
 }*/
 
-//EXERCISE 3 - MONTH/SEASON LOGIC
+/*EXERCISE 3 - MONTH/SEASON LOGIC
 Console.WriteLine("Select your birthday Month from the list below.");
 Console.WriteLine("(1) January\n(2) February\n(3) March\n(4) April\n(5) May\n(6) June\n(7) July\n(8) August\n(9) September\n(10) October\n(11) November\n(12) December");
 Console.Write("Enter your birth month (eg: '1' for January): ");
@@ -60,4 +60,41 @@ switch (birthMonth)
     case 9 or 10 or 11:
         Console.WriteLine("Your birthday is in Spring!");
         break;
+}*/
+
+//EXERCISE 4 - TIMES TABLES
+Console.WriteLine("MULTIPLICATION CALCULATOR");
+int firstNumber;
+int secondNumber;
+
+while (true)
+{
+    Console.Write("Enter 1st Number (0 - 12): ");
+    firstNumber = int.Parse(Console.ReadLine());
+    if (firstNumber >= 0 && firstNumber <= 12)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid number, please enter a number between 0 and 12.");
+    }
 }
+
+while (true)
+{
+    Console.Write("Enter 2nd Number (0 - 12): ");
+    secondNumber = int.Parse(Console.ReadLine());
+    if (secondNumber >= 0 && secondNumber <= 12)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid number, please enter a number between 0 and 12.");
+    }
+}
+
+int result = firstNumber * secondNumber;
+Console.WriteLine("Request: {0} x {1}", firstNumber, secondNumber);
+Console.WriteLine("Result: {0}", result);
